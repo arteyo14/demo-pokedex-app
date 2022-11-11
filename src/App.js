f2ed46d71e2ff70bd8c527b7e7749647df03561c
arteyo14 <98311957+arteyo14@users.noreply.github.com>
2022-11-12 01:04:12 +0700

    [02-setup] - restyling and set up background project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { Button } from 'antd';
+import { Button, Image } from 'antd';
+
+import pokemonLogoSrc from './assets/images/pokedex.png';
 
 function App() {
   return (
     <div>
       <div>React App</div>
+      <Image width={200} src={pokemonLogoSrc} />
       <Button type="primary">Click me</Button>
     </div>
   );
