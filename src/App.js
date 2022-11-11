7b315bd21a8ec4f69a0e8b2d854490c9c236a172
arteyo14 <98311957+arteyo14@users.noreply.github.com>
2022-11-12 00:44:38 +0700

    [02-setup] - install ui dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Button } from 'antd';
+
 function App() {
-  return <div>React App</div>;
+  return (
+    <div>
+      <div>React App</div>
+      <Button type="primary">Click me</Button>
+    </div>
+  );
 }
 
 export default App;
