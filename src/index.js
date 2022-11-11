09dbbb2f1b86aa0bba125409987a43bafb825fcc
arteyo14 <98311957+arteyo14@users.noreply.github.com>
2022-11-12 00:47:46 +0700

    [02-setup] - adding asset

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
